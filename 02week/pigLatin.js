58fc51e155e2f9882f611217178c911068929ede
Casey O'Keefe <the0surfer@aol.com>
2017-07-17 21:18:44 -0500

    logic for piglatin program added

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -7,17 +7,62 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-
-function pigLatin(word) {
-
-  // Your code here
-
+function pigLatin (word) {
+  word = word.toLowerCase().trim();
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  var vowelNum = word.length;
+  var first;
+  var last;
+  for (var i = 0; i < vowels.length; i++) {
+    if (word.indexOf(vowels[i]) > -1) {
+      if (word.indexOf(vowels[i]) < vowelNum) {
+        vowelNum = word.indexOf(vowels[i]);
+      } // findest closet number of vowel
+    } // finds if the vowel exists in the word
+  }
+  if (vowelNum !== 0) {
+    first = word.slice(vowelNum, word.length);
+    last = word.slice(0, vowelNum);
+    return (first + last) + 'ay';
+  } else {
+    return word + 'yay';
+  }
 }
 
+/*
+function pigLatin (word) {
 
-function getPrompt() {
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  word = word.toLowerCase().trim();
+  let finalWord = word;
+  for (var i = 0; i < word.length; i++) {
+    if (checkVowel(word[i])) {
+      // starts with vowel if statement
+      if (i === 0) {
+        finalWord = finalWord + 'yay';
+      } else {
+        finalWord = finalWord + 'ay';
+      }
+      return finalWord;
+    } else {
+      finalWord = finalWord.substring(1);
+      finalWord += word[i];
+    }
+  }
+}
+
+function checkVowel (letter) {
+  if ((letter === 'a') || (letter === 'e') || (letter === 'i') || (letter === 'o') || (letter === 'u')) {
+    return true;
+  } else {
+    return false;
+  }
+}
+*/
+function getPrompt () {
   rl.question('word ', (answer) => {
-    console.log( pigLatin(answer) );
+    console.log(pigLatin(answer));
     getPrompt();
   });
 }
@@ -25,7 +70,6 @@ function getPrompt() {
 // Tests
 
 if (typeof describe === 'function') {
-
   describe('#pigLatin()', () => {
     it('should translate a simple word', () => {
       assert.equal(pigLatin('car'), 'arcay');
