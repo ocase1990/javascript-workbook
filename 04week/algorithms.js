052b022ad48f8e6dd90cb5265306e966ac6b37ab
Casey O'Keefe <the0surfer@aol.com>
2017-07-31 20:34:18 -0500

    algs done

--- 04week/algorithms.js
+++ 04week/algorithms.js
@@ -2,7 +2,7 @@
 
 const assert = require('assert');
 
-function getRandomInt(min, max) {
+function getRandomInt (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
@@ -14,20 +14,63 @@ for (let i = 0; i < 1000; i++) {
   arr.push(getRandomInt(0, 1000));
 }
 
-function bubbleSort(arr) {
-  // Your code here
+function bubbleSort (arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      let temp = arr[i];
+      arr[i] = arr[i + 1];
+      arr[i + 1] = temp;
+      i = -1;
+    }
+  }
+  return arr;
 }
 
-function mergeSort(arr) {
-  // Your code here
+function mergeSort (arr) {
+  if (arr.length < 2) return arr;
+  var middle = Math.floor(arr.length / 2);
+  var left = arr.slice(0, middle);
+  var right = arr.slice(middle, arr.length);
+  return merge(mergeSort(left), mergeSort(right));
 }
 
-function binarySearch(arr, item) {
-  // Your code here
+function merge (left, right) {
+  var result = [];
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+  while (left.length) result.push(left.shift());
+
+  while (right.length) result.push(right.shift());
+
+  return result;
+}
+
+function binarySearch (arr, item) {
+  var minIndex = 0;
+  var maxIndex = arr.length - 1;
+  var currentIndex;
+  var currentElement;
+
+  while (minIndex <= maxIndex) {
+    currentIndex = (minIndex + maxIndex) / 2 | 0;
+    currentElement = arr[currentIndex];
+    if (currentElement < item) {
+      minIndex = currentIndex + 1;
+    } else if (currentElement > item) {
+      maxIndex = currentIndex - 1;
+    } else {
+      return currentIndex;
+    }
+  }
+  return false;
 }
 
 // Tests
-
 if (typeof describe === 'function') {
 
   function comparator(a, b) {
